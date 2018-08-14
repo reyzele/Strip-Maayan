dacf81d3fcd24692af4126d6b43b76bb66a75d62
Vadim Veksler <reyzele91@gmail.com>
2018-08-14 18:47:31 +0300

    .

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,16 @@
+// Proto
+
 const Proto = document.querySelector("#Proto");
 const protoWrap = document.querySelector(".proto-wrap");
 const proto_ = document.querySelector(".protoio-embed-prototype");
 const typography = document.querySelector(".typography");
 const phoneImage = document.querySelector(".typography__img");
 
-phoneImage.style.display = "none";
-
 window.onload = function() {
   proto_.style.display = "none";
   proto_.style.opacity = "1";
-  phoneImage.style.display = "inherit";
+
+  preloader_();
 
   if (
     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
@@ -46,4 +47,32 @@ function handleMobile(e) {
   proto_.classList.add("phones");
   proto_.style.height = "auto";
   proto_.style.top = "0";
+}
+
+// Preloader
+
+let images = document.images,
+  images_total_count = images.length,
+  images_loaded_count = 0,
+  preloader = document.getElementById("page-preloader"),
+  perc_display = document.getElementById("load_perc");
+
+for (var i = 0; i < images_total_count; i++) {
+  var image_clone = new Image();
+  image_clone.onload = image_loaded;
+  image_clone.onerror = image_loaded;
+  image_clone.src = images[i].src;
+}
+function image_loaded() {
+  images_loaded_count++;
+  perc_display.innerHTML =
+    (((100 / images_total_count) * images_loaded_count) << 0) + "%";
+}
+function preloader_() {
+  if (images_loaded_count >= images_total_count) {
+    if (!preloader.classList.contains("done")) {
+      preloader.classList.add("done");
+      document.body.classList.remove("fixed");
+    }
+  }
 }
